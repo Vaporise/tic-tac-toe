3ce5d227d4f5386993b3e062071da2151bf74255
Vaporise <draxdragonfire@googlemail.com>
2025-04-13 15:25:18 +0900

    commented out my psuedocode

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+import random
 
+board_width = 3
+board_height = 3
 
 def new_board():
-         tic_tac_toe_board = [
-        [None,None,None],
-        [None,None,None],
-        [None,None,None]
-    ]
-         return tic_tac_toe_board
+    board = []
+    for x in range(0, board_width):
+        column = []
+        for y in range(0, board_height):
+            column.append(None)
+        board.append(column)
+    return board
 
 board = new_board()
+
+print(board)
 
 # Loop thrrugh turns
 #loop:
@@ -17,33 +23,33 @@ board = new_board()
     #current_player = pass
 # Print the current board state
 
-def render():
+#def render():
      
 
-render(board)
+#render(board)
 
 # Get the input for the current players move
-move_co_ords = get_move()
+#move_co_ords = get_move()
 
 # make the move that was recieved with get_move()
 
-make_move(board, move_co_ords, current_player)
+#make_move(board, move_co_ords, current_player)
 
 # Check for a winner
 
-winner = get_winner(board)
+#winner = get_winner(board)
 
 #If there is a winner, declare the winner and exit loop
 
-if winner is not None:
-    print(f"The Winner is {winner}!!!!")
-    break
+#if winner is not None:
+    #print(f"The Winner is {winner}!!!!")
+   # break
 
 #If there is no winner and the board is full
 #exit loop and declare a draw
 
-if is_board_full(board):
-    print("IT'S A DRAW!!!!!")
-    break
+#if is_board_full(board):
+    #print("IT'S A DRAW!!!!!")
+    #break
 
 #Repeat until the game is over
