0cbfaab92424c31d1456cede5d484a14da7d9762
Vaporise <draxdragonfire@googlemail.com>
2025-04-15 22:02:25 +0900

    updates

--- main.py
+++ main.py
@@ -1,55 +1,20 @@
-import random
+import tkinter as tk
+from tkinter import font
 
-board_width = 3
-board_height = 3
+class TicTacToeBoard(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Tic-Tac-Toe Game") # creates the title for the game window
+        self._cells = {} #will contain the co-ordinates for the tic-tac-toe game
 
-def new_board():
-    board = []
-    for x in range(0, board_width):
-        column = []
-        for y in range(0, board_height):
-            column.append(None)
-        board.append(column)
-    return board
+    def _create_board_display(self):
+        display_frame = tk.Frame(master=self) # creates a display frame
+        display_frame.pack(fill=tk.X) #Puts the frame on the main window's top border
+        self.display = tk.Label(
+            master=display_frame,
+            text="Ready?",
+            font = font.Font(size=28, weight="bold"),
+        )
+        self.display.pack() #packs the above label inside the frame.
 
-board = new_board()
-
-print(board)
-
-# Loop thrrugh turns
-#loop:
-    #TODO: need to create infinite loop
-    # Need to select current player
-    #current_player = pass
-# Print the current board state
-
-#def render():
-     
-
-#render(board)
-
-# Get the input for the current players move
-#move_co_ords = get_move()
-
-# make the move that was recieved with get_move()
-
-#make_move(board, move_co_ords, current_player)
-
-# Check for a winner
-
-#winner = get_winner(board)
-
-#If there is a winner, declare the winner and exit loop
-
-#if winner is not None:
-    #print(f"The Winner is {winner}!!!!")
-   # break
-
-#If there is no winner and the board is full
-#exit loop and declare a draw
-
-#if is_board_full(board):
-    #print("IT'S A DRAW!!!!!")
-    #break
-
-#Repeat until the game is over
+    def _create_board_grid(self:)
