add52b1a95448f4be60417b4624aa867d1a4237c
Vaporise <draxdragonfire@googlemail.com>
2025-04-15 22:11:35 +0900

    added code to get the main tic-tac-toe board up and running

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ class TicTacToeBoard(tk.Tk):
         super().__init__()
         self.title("Tic-Tac-Toe Game") # creates the title for the game window
         self._cells = {} #will contain the co-ordinates for the tic-tac-toe game
+        self._create_board_display()
+        self._create_board_grid()
 
     def _create_board_display(self):
         display_frame = tk.Frame(master=self) # creates a display frame
@@ -17,4 +19,35 @@ class TicTacToeBoard(tk.Tk):
         )
         self.display.pack() #packs the above label inside the frame.
 
-    def _create_board_grid(self:)
+    def _create_board_grid(self):
+        grid_frame = tk.Frame(master=self) #create the grid's frame
+        grid_frame.pack()
+        for row in range(3): #iterates over 3 in a row
+            self.rowconfigure(row, weight=1, minsize=50)
+            self.columnconfigure(row, weight=1, minsize=75)
+            for col in range(3): #iterates over 3 in a column
+                button = tk.Button( #creates a button on each grid slot
+                    master=grid_frame,
+                    text="",
+                    font=font.Font(size=36, weight="bold"),
+                    fg="black",
+                    width=3,
+                    height=2,
+                    highlightbackground="lightblue",
+                )
+                self._cells[button] = (row,col) #adds every new button to the cell dictionary created in the __init__.
+                button.grid(
+                    row=row,
+                    column=col,
+                    padx=5,
+                    pady=5,
+                    sticky="nsew"
+                )
+
+    def main():
+        """ create  the game loop"""
+        board = TicTacToeBoard()
+        board.mainloop()
+
+    if __name__ == "__main__":
+        main()
